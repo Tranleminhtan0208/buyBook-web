33e7da491aa4363c6f273d8c4a4818cd55a08afa
ltnamptitit <ltnamptit@gmail.com>
2023-09-22 11:33:13 +0700

    Update Login.js
    add state

--- buy-book/src/components/Login.js
+++ buy-book/src/components/Login.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import LoginHeader from "./LoginHeader";
 import "./style.css";
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    };
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
+    const onClickLogin = (e) => {
+        if (email.length !== 0 && password.length !== 0) {
+            e.preventDefault();
+            console.log({
+                email: email,
+                password: password
+            });
+            setEmail("");
+            setPassword("");
+        }
+        else{
+            alert("username or password is empty")
+        }
+    };
     return (
         <>
             <LoginHeader />
             <div className="flex-grow-1 centered">
                 <div className="form-container">
-                    <form className="p-3 d-flex flex-column align-items-center">
+                    <form className="p-3 d-flex flex-column align-items-center mb-3">
                         <h4 className="w-100 text-center">Đăng nhập</h4>
                         <a
                             href="https://www.facebook.com/no.xungthien"
@@ -35,25 +58,38 @@ function Login() {
                         <div className="w-100">
                             <input
                                 type="email"
-								name="email"
-								id="email"
+                                id="email"
                                 placeholder="Email"
                                 autoComplete="false"
                                 className="w-100 border rounded px-3 input"
+                                value={email}
+                                onChange={(e) => onChangeEmail(e)}
                             />
                         </div>
                         <div className="w-100">
                             <input
                                 type="password"
+                                id="password"
                                 placeholder="Password"
                                 autoComplete="false"
                                 className="w-100 border rounded px-3 input"
-								name="password"
-								id="password"
+                                value={password}
+                                onChange={(e) => onChangePassword(e)}
                             />
                         </div>
-						<button className="w-100 bg-primary rounded border-0 py-2 login-button">Đăng nhập</button>
+                        <button
+                            className="w-100 bg-primary rounded border-0 py-2 login-button"
+                            onClick={(e) => onClickLogin(e)}
+                        >
+                            Đăng nhập
+                        </button>
                     </form>
+                    <div className="border-top my-4 text-center">
+                        Bạn chưa có tài khoản?{" "}
+                        <a href="#" className="text-primary">
+                            Đăng ký ngay
+                        </a>
+                    </div>
                 </div>
             </div>
         </>
